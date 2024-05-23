9e53489d6996afba0fdfabcee7e40cdaad2742bf
Maria Julia Germano <166447580+MariaJuliaGermano@users.noreply.github.com>
2024-05-22 21:56:30 -0300

    Update atividadejson.py

--- atividadejson.py
+++ atividadejson.py
@@ -1,9 +1,11 @@
 import json
+from time import sleep
 
 def nome_sistema():
     print("="*10, end="")
     print("SISTEMA DE CADASRO", end="")
     print("="*10)
+
 
 def menu():
     print("-="*7)
@@ -11,6 +13,7 @@ def menu():
     print("2. Listar Clientes")
     print("3. Excluir Clientes")
     print("4. Sair")
+
 
 def escolha():
     escolha = int(input("Escolha uma das opções: "))
@@ -25,10 +28,11 @@ def escolha():
             print("Obrigada, volte sempre!")
             exit()
 
+
 def cadastrar():
-    nome = str(input("Digite o seu nome: "))
+    nome = str(input("Digite o seu nome: ")).strip()
     idade = int(input("Digite a sua idade: "))
-    email = str(input("Digite o seu e-mail: "))
+    email = str(input("Digite o seu e-mail: ")).strip()
 
     usuario = {
         "nome": nome,
@@ -36,28 +40,46 @@ def cadastrar():
         "email": email
     }
 
-    with open("atividadejson.json", "w") as arquivo:
-        json.dump(usuario, arquivo, indent=4)
+    with open("atividade.json", "r") as arquivo:
+        usuarios = json.load(arquivo)
+
+    usuarios.append(usuario)
+
+    with open("atividade.json", "w") as arquivo:
+        json.dump(usuarios, arquivo, indent=4)
+        arquivo.write("\n")
+    
+    print("Cliente Cadastrado com sucesso!")
+
 
 def listar():
-    with open("atividadejson.json", "r") as arquivo:
+    with open("atividade.json", "r") as arquivo:
         dados = json.load(arquivo)
-    print(dados)
+        if dados:
+            print("Listando clientes cadasrtados")
+            for linha in dados:
+                print(linha)
+
 
 def excluir():
-    nome = input("Digite o nome da pessoa que deseja excluir: ").lower()
-    with open('atividadejson.json', 'r') as arquivo:
+    nome = input("Digite o nome da pessoa que deseja excluir: ").strip()
+    with open('atividade.json', 'r') as arquivo:
         dados = json.load(arquivo)
 
     for pessoa in dados:
-        if dados['nome'] == nome:
+        if pessoa['nome'].lower() == nome.lower():
             dados.remove(pessoa)
+
+    with open("atividade.json", "w") as arquivo:
+        json.dump(dados, arquivo, indent=4)
+            
 
 def main():
     nome_sistema()
     while True:
         menu()
         escolha()
+
 
 if __name__ == "__main__":
     main()
